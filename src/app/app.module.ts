7f8a4696ad70749cfb24d96dac4748042c71b7f2
Dey <Souvik.Dey@thomsonreuters.com>
2018-06-05 11:29:23 +0530

    Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ItemsComponent } from './components/items/items.component';
+import { ItemComponent } from './components/items/item/item.component';
+import { ItemListComponent } from './components/items/item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ItemsComponent,
+    ItemComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule
